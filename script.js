f3df83cb08836e6fe4aa2ef490a80a3f51806e68
houssamelbadaoui <helbad@aulaestudio.es>
2026-01-08 19:10:58 +0100

    Add reset button

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ function getRandomColor() {
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
+// Add eventListener to the resetButton
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Enter new grid size (e.g 16 for 16x16):"));
+    grid.innerHTML = ""; //remove all children;
+    //rebuild the grid
+    for (let i = 0; i < newSize * newSize; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = getRandomColor();
+        });
+        grid.appendChild(div);
+    }
+    grid.style.gridTemplateColums = `repeat(${newSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+});

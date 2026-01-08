21e9214dc48c1b043db82db9b30645d76d112b68
houssamelbadaoui <helbad@aulaestudio.es>
2026-01-08 18:26:11 +0100

    Add style for the grid and container

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const grid = document.querySelector("#container");
+//create 256 divs inside the container
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+}
+
